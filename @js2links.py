84e52c6c3a0c6bac7ddc35dab591efe28b150d96
wqy88 <wqy88@163.com>
2019-10-26 21:28:34 +0800

    这是2019-10-26_21:28的维护日志，由wqy88程序自动创建

--- @js2links.py
+++ @js2links.py
@@ -4,13 +4,15 @@ import json
 from lxml import etree
 
 class Spider:
-    def __init__(self, headers, url,fp=None):
+    def __init__(self, headers, url, fn, fp=None):
         self.headers = headers
         self.url=url
+        self.fn=fn
         self.fp=fp
 
     def open_file(self):
-        self.fp = open('学习强国2.txt', 'w', encoding='utf8')
+        #self.fp = open('学习强国2.txt', 'w', encoding='utf8')
+        self.fp = open(self.fn, 'w', encoding='utf8')
 
     def get_data(self):
        return requests.get(url=self.url, headers=self.headers).text
@@ -31,7 +33,7 @@ class Spider:
         self.fp.write(urls[:-1])
         print(i)'''
         print(len(detail_url))
-        self.fp.write('["' + '","'.join(detail_url) + '"]') #构造一个list的文本格式
+        self.fp.write(f'["' + '","'.join(detail_url) + '"]') #构造一个list的文本格式
 
     def close_file(self):
         self.fp.close()
@@ -46,10 +48,18 @@ if __name__ == '__main__':
         'Host': 'www.xuexi.cn',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
     }
-    #url = 'https://www.xuexi.cn/f997e76a890b0e5a053c57b19f468436/data018d244441062d8916dd472a4c6a0a0b.js' #练习用
-    #url = 'https://www.xuexi.cn/lgdata/35il6fpn0ohq.json' #文章数量
-    #url = 'https://www.xuexi.cn/72ac54163d26d6677a80b8e21a776cfa/data9a3668c13f6e303932b5e0e100fc248b.js' #文章时长
-    #url = 'https://www.xuexi.cn/lgdata/17th9fq5c7l.json' #视频数量
-    url = 'https://www.xuexi.cn/a191dbc3067d516c3e2e17e2e08953d6/datab87d700beee2c44826a9202c75d18c85.js'
-    spider=Spider(url=url,headers=headers)
+    url0 = 'https://www.xuexi.cn/f997e76a890b0e5a053c57b19f468436/data018d244441062d8916dd472a4c6a0a0b.js' #练习用
+    
+    url1 = 'https://www.xuexi.cn/lgdata/35il6fpn0ohq.json' #文章数量
+    url2 = 'https://www.xuexi.cn/72ac54163d26d6677a80b8e21a776cfa/data9a3668c13f6e303932b5e0e100fc248b.js' #文章时长
+    url3 = 'https://www.xuexi.cn/lgdata/17th9fq5c7l.json' #视频数量
+    url4 = 'https://www.xuexi.cn/a191dbc3067d516c3e2e17e2e08953d6/datab87d700beee2c44826a9202c75d18c85.js' #视频时长
+    
+    spider=Spider(url=url0,headers=headers,fn='tmp.txt')
+    
+    #spider=Spider(url=url2,headers=headers,fn='article_links_new.db')
+    #spider=Spider(url=url3,headers=headers,fn='article_links_old.db')
+    #spider=Spider(url=url4,headers=headers,fn='video_links_new.db')
+    #spider=Spider(url=url1,headers=headers,fn='video_links_old.db')
+    
     spider.run()
